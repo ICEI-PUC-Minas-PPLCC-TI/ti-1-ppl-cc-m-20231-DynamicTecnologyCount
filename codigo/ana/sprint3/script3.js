82885bf946a7e648a70798ef8c54f160931de173
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2023-06-26 17:55:35 -0300

    Add files via upload

--- codigo/ana/sprint3/script3.js
+++ codigo/ana/sprint3/script3.js
@@ -1,42 +1,44 @@
-var pacientes = [
-  { id: 1, nome: 'Paciente 1', escala: 'alta', ala: 'Ala A' },
-  { id: 2, nome: 'Paciente 2', escala: 'baixa', ala: 'Ala B' },
-  { id: 3, nome: 'Paciente 3', escala: 'alta', ala: 'Ala A' },
-  { id: 4, nome: 'Paciente 4', escala: 'media', ala: 'Ala C' },
-  { id: 5, nome: 'Paciente 5', escala: 'baixa', ala: 'Ala B' },
-];
-
-var tabelaPacientes = document.querySelector('.tabela-pacientes');
-
-for (var i = 0; i < pacientes.length; i++) {
-  var paciente = pacientes[i];
-
-  var linha = document.createElement('div');
-  linha.classList.add('linha');
-
-  var id = document.createElement('div');
-  id.classList.add('id-paciente');
-  id.textContent = paciente.id;
-
-  var nome = document.createElement('div');
-  nome.classList.add('nome-paciente');
-  nome.textContent = paciente.nome;
-
-  var escala = document.createElement('div');
-  escala.classList.add('escala-fugulin');
-  escala.textContent = paciente.escala;
-
-  var ala = document.createElement('div');
-  ala.classList.add('ala-hospital');
-  ala.textContent = paciente.ala;
-
-  linha.appendChild(id);
-  linha.appendChild(nome);
-  linha.appendChild(escala);
-  linha.appendChild(ala);
-
-  tabelaPacientes.appendChild(linha);
+function getInformacoes() {
+  return JSON.parse(localStorage.getItem('informacoes')) || [];
 }
+
+// Função para exibir as informações na tabela
+function exibirInformacoes() {
+  var informacoes = getInformacoes();
+  var tabela = document.getElementById('tabelaCRUD');
+
+  // Limpar tabela
+  tabela.getElementsByTagName('tbody')[0].innerHTML = '';
+
+  for (var i = 0; i < informacoes.length; i++) {
+    var informacao = informacoes[i];
+
+    var linha = document.createElement('tr');
+    var colunaNome = document.createElement('td');
+    var colunaHoras = document.createElement('td');
+    var colunaSalario = document.createElement('td');
+    var colunaTotal = document.createElement('td');
+
+    colunaNome.textContent = informacao.nome;
+    colunaHoras.textContent = informacao.horas;
+    colunaSalario.textContent = informacao.salario;
+    colunaTotal.textContent = informacao.total;
+    colunaCargo.textContent = informacao.cargo;
+
+
+
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaHoras);
+    linha.appendChild(colunaSalario);
+    linha.appendChild(colunaTotal);
+
+
+    tabela.getElementsByTagName('tbody')[0].appendChild(linha);
+
+  }
+}
+
+exibirInformacoes();
 
 function buscar() {
   var termoBusca = document.getElementById('termo-busca').value.toLowerCase();
